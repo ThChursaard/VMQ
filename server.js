9dac4eddcaba558610c90d3fc1efb3b1bb22eb43
ThChursaard <jame.paladin26222@gmail.com>
2021-10-08 18:53:05 +0700

    fixanything

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 //const socketIo = require("socket.io");
 var cors = require("cors");
 let scoreList = [];
-let songList = [];
+let questionList = [];
 // use it before all route definitions
 
 const uniqueArray = (a) =>
